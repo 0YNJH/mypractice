02f33fdc2d0fbc938d7416042d5668defdbf1f12
0YNJH <106074753+0YNJH@users.noreply.github.com>
2023-01-22 18:05:43 +0900

    pythonpractice
    solve baekjoon problem

---
+++ baekjoon.py
@@ -0,0 +1,16 @@
+# baekjoon 10807
+# n=int(input())
+# lis = list(map(int,input().split()))
+# v=int(input())
+# print(lis.count(v))
+
+# baekjoon 10871
+# n,x=map(int,input().split())
+# a=list(map(int,input().split()))
+# for i in a:
+#     if  (i<x): print("%d"%i,end=" ")
+
+# baekjoon 10818
+n=int(input())
+n_list=list(map(int,input().split()))
+print(min(n_list),max(n_list))
