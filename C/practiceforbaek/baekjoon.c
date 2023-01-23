f2f8c6c7a877f99a7a3444706ead65e7f6e5533e
0YNJH <106074753+0YNJH@users.noreply.github.com>
2023-01-23 22:44:20 +0900

    d2
    d

--- C/practiceforbaek/baekjoon.c
+++ C/practiceforbaek/baekjoon.c
@@ -19,22 +19,63 @@
 // }
 
 // 백준 3052
-#include <stdio.h>
-int main(){
-    int a[10]={0,};
-    for(int i=0;i<10;i++){
-        scanf("%d",&a[i]);
-        a[i]=a[i]%42;
-    }
+// #include <stdio.h>
+// int main(){
+//     int a[10]={0,};
+//     for(int i=0;i<10;i++){
+//         scanf("%d",&a[i]);
+//         a[i]=a[i]%42;
+//     }
 
-    int cnt_1=0;
-    for(int i=0;i<10;i++){
-        int cnt=0;
-        for(int j=0;j<10;j++){
-            if(a[j]==a[i]) ++cnt;
-        }
-        if(cnt==0) ++cnt_1;
-    }
+//     int cnt_1=0;
+//     for(int i=0;i<10;i++){
+//         int cnt=0;
+//         for(int j=0;j<10;j++){
+//             if(a[j]==a[i]) ++cnt;
+//         }
+//         if(cnt==0) ++cnt_1;
+//     }
    
-    printf("%d",cnt_1);
-}
+//     printf("%d",cnt_1);
+// }
+
+// #include <stdio.h>
+// int main(void) {
+
+//     int in;
+
+//     int c = 0;
+
+//     int a[10];
+
+//     for (int i = 0; i < 10; i++) {
+
+//         scanf("%d", &in);
+
+//         a[i] = (in % 42);
+
+//     }
+
+//     for (int i = 0; i < 10; i++) {
+
+//         int num = 0;
+
+//         for (int j = i + 1; j < 10; j++) {
+
+//             if (a[i] == a[j])
+
+//                 num++;
+
+//         }
+
+//         if (num == 0)
+
+//             c++;
+
+//     }
+
+//     printf("%d", c);
+
+//     return 0;
+
+// }
