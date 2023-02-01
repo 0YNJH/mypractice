dc4f5f1dfbd592be3ee2dcf7894b9afda237e4f8
0YNJH <106074753+0YNJH@users.noreply.github.com>
2023-02-01 15:32:41 +0900

    230201-1
    c 동적할당2

--- C/practiceforbaek/baekjoon.c
+++ C/practiceforbaek/baekjoon.c
@@ -276,3 +276,21 @@
 // 	}
 // 	printf("%d", max - min);
 // }
+
+// 백준 17608
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	int n, max = 0, total = 0;
+	scanf("%d", &n);
+	int* stick = (int*)malloc(sizeof(int*) * n);
+	for (int i = 0; i < n; i++) scanf("%d", &stick[i]);
+	for (int i = n - 1; i >= 0; i--) {
+		if (max < stick[i]) {
+			max = stick[i];
+			total += 1;
+		}
+	}
+	printf("%d", total);
+	return 0;
+}
